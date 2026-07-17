7de3dd3f808a061b62c479c56dba6da7507be72e
FranciscoNeves15_Desktop <francisconeves15.dev@gmail.com>
2026-07-17 12:28:52 -0300

    new info 2

--- data/data.ts
+++ data/data.ts
@@ -24,5 +24,17 @@ const data: ChestHuntingPattern[][] = [
     { position: 22, content: "loot" }, { position: 23, content: "loot" }, { position: 24, content: "monster" },
     { position: 25, content: "loot" }, { position: 26, content: "loot" }, { position: 27, content: "protector" },
     { position: 28, content: "loot" }, { position: 29, content: "loot" }, { position: 30, content: "loot" }
+  ],
+  [
+    { position: 1, content: "loot" }, { position: 2, content: "monster" }, { position: 3, content: "monster" },
+    { position: 4, content: "loot" }, { position: 5, content: "loot" }, { position: 6, content: "loot" },
+    { position: 7, content: "loot" }, { position: 8, content: "loot" }, { position: 9, content: "loot" }, 
+    { position: 10, content: "loot" }, { position: 11, content: "protector" }, { position: 12, content: "loot" },
+    { position: 13, content: "loot" }, { position: 14, content: "loot" }, { position: 15, content: "loot" },
+    { position: 16, content: "loot" }, { position: 17, content: "loot" }, { position: 18, content: "loot" },
+    { position: 19, content: "loot" }, { position: 20, content: "loot" }, { position: 21, content: "loot" },
+    { position: 22, content: "loot" }, { position: 23, content: "loot" }, { position: 24, content: "loot" },
+    { position: 25, content: "loot" }, { position: 26, content: "monster" }, { position: 27, content: "protector" },
+    { position: 28, content: "monster" }, { position: 29, content: "loot" }, { position: 30, content: "loot" }
   ]
 ];
